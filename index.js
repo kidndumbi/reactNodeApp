1cb24384364a6ef9fb9346811dc224642ad0aaa6
Cassava <kidndumbi@gmail.com>
2017-09-13 00:27:02 -0400

    passport and google strategy

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategey = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
+passport.use(new GoogleStrategey());
 
-app.get('/', (req, res) => {
 
-    res.send({hi: 'there'});
-
-});
-
-app.get('/greedo', (req, res) => {
-
-    res.send({greedo: 'I shot first'});
-
-});
+// app.get('/', (req, res) => {
+//
+//     res.send({hi: 'there'});
+//
+// });
+//
 
 const PORT = process.env.PORT || 5000;
 
