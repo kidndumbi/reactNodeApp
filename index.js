25bc8eb604b75c92d68ade58e27250d2419f60a5
Cassava <kidndumbi@gmail.com>
2017-09-19 02:03:56 -0400

    passport serializer

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dbConnect = require('./config/mongodb');
+
+require('./dbModels/User');
 require('./services/passport');
 
-const app = express();
+mongoose.connect(dbConnect.mongoURI);
+
+
+
+const app = express('');
 require('./routes/authRoutes')(app);
 
 
--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,22 @@ const passport = require('passport');
 const GoogleStrategey = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys')
 
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+     
+     done(null, user.id);
+
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+         done(null, user);
+    });
+});
+
 
 passport.use(new GoogleStrategey({
     clientID: keys.googleClientID,
@@ -9,8 +25,23 @@ passport.use(new GoogleStrategey({
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshtoken, profile, done) => {
 
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshtoken);
-        console.log('profile', profile);
+        //console.log('profile', profile);
+
+        User.findOne({
+            googleId: profile.id
+        }).then((existingUser) => {
+              if(existingUser){
+                  console.log('user already exists');
+                  done(null, existingUser);
+              } else{
+                new User({
+                        googleId: profile.id
+                    }).save().then((userSaved) => {
+                        done(null, userSaved);
+                    });
         //console.log('done', done);
+              }
+        })
+
+    
 }));
